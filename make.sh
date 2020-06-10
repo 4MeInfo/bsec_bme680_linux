89b9f2c0349bcc1e5f72180b054605502dfa8a92
4MeInfo <53448222+4MeInfo@users.noreply.github.com>
2020-06-10 16:38:26 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -18,9 +18,9 @@ fi
 #cp ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/avr/AVR8_megaAVR/libalgobsec.a ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/libalgobsec.a
 #cp ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/inc/bsec_datatypes.h ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/bsec_datatypes.h
 #cp ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/inc/bsec_interface.h ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/bsec_interface.h
-cp ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/bsec_interface.h ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/bsec_interface.h
-cp ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/bsec_datatypes.h ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/bsec_datatypes.h
-cp ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/libalgobsec.a ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/libalgobsec.a
+cp /tmp/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/bsec_interface.h ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/bsec_interface.h
+cp /tmp/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/bsec_datatypes.h ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/bsec_datatypes.h
+cp /tmp/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/libalgobsec.a ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/examples/libalgobsec.a
 
 echo 'Compiling...'
 cc -std=c99 -std=gnu99 -std=c11 -std=gnu11 -Wall -Wno-unused-but-set-variable -Wno-unused-variable -static \
@@ -30,7 +30,7 @@ cc -std=c99 -std=gnu99 -std=c11 -std=gnu11 -Wall -Wno-unused-but-set-variable -W
   "${BSEC_DIR}"/API/bme680.c \
   "${BSEC_DIR}"/examples/bsec_integration.c \
   ./bsec_bme680.c \
-  -L ~/Downloads/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/ -lalgobsec \
+  -L /tmp/bsec_bme680_linux-master/.src/BSEC_1.4.7.4_Generic_Release/algo/normal_version/bin/RaspberryPI/PiZero_ArmV6-32bits/ -lalgobsec \
   -lm -lrt \
   -o bsec_bme680
 echo 'Compiled.'
